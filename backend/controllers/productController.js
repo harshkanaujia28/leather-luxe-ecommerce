71f3f8cf570e5c716e46591908ed61b37252bb4c
harshkanaujia28 <154357957+harshkanaujia28@users.noreply.github.com>
2025-10-20 02:00:54 +0530

    changes

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,4 +1,4 @@
-import Product from "../models/product.js";
+import Product from "../models/Product.js";
 import { uploadToCloudinary } from "../utils/cloudinary.js";
 
 // --- Safe parsing helpers ---
